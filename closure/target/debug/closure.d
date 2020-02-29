76734972dcbb628fc0b124a73b67ad5ac5fe701e
dyike <yuanfeng634@gmail.com>
2020-02-29 16:25:26 +0800

    add closure

---
+++ closure/target/debug/closure.d
@@ -0,0 +1,1 @@
+/Users/ityike/Code/rust/playgroud.rs/closure/target/debug/closure: /Users/ityike/Code/rust/playgroud.rs/closure/src/main.rs
---
+++ closure/target/debug/deps/closure-735c26c156259588.d
@@ -0,0 +1,5 @@
+/Users/ityike/Code/rust/playgroud.rs/closure/target/debug/deps/closure-735c26c156259588: src/main.rs
+
+/Users/ityike/Code/rust/playgroud.rs/closure/target/debug/deps/closure-735c26c156259588.d: src/main.rs
+
+src/main.rs:
