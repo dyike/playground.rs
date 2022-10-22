c671f28df53efee1c597ba7337c3c6c988298302
dyike <yuanfeng634@gmail.com>
2022-10-22 22:03:29 +0800

    fix typo

--- data-struct-fun/target/debug/deps/data_struct_fun-6c832d5fa180dc48.d
+++
@@ -1,5 +0,0 @@
-/Users/ityike/Code/rust/playgroud.rs/data-struct-fun/target/debug/deps/data_struct_fun-6c832d5fa180dc48.rmeta: src/main.rs
-
-/Users/ityike/Code/rust/playgroud.rs/data-struct-fun/target/debug/deps/data_struct_fun-6c832d5fa180dc48.d: src/main.rs
-
-src/main.rs:
--- data-struct-fun/target/debug/deps/data_struct_fun-df1d246022911088.d
+++
@@ -1,5 +0,0 @@
-/Users/ityike/Code/rust/playgroud.rs/data-struct-fun/target/debug/deps/data_struct_fun-df1d246022911088.rmeta: src/main.rs
-
-/Users/ityike/Code/rust/playgroud.rs/data-struct-fun/target/debug/deps/data_struct_fun-df1d246022911088.d: src/main.rs
-
-src/main.rs:
